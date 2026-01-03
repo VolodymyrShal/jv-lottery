df4cfe978f6eeaeebfb36d6c1e6a61451dce5842
Volodymyr <volodymyrshalata@ukr.net>
2026-01-03 21:11:09 +0100

    fixed checkstyle errors

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -4,7 +4,7 @@ public class Application {
     public static void main(String[] args) {
         // create three balls using class Lottery and print information about them in console
         Ball ballOne = Lottery.getRandomBall();
-        Ball ballTwo =  Lottery.getRandomBall();
+        Ball ballTwo = Lottery.getRandomBall();
         Ball ballThree = Lottery.getRandomBall();
 
         System.out.println(ballOne);
