2fdc7a4d7d3a39916c3097383543f08d06096059
Volodymyr <volodymyrshalata@ukr.net>
2026-01-03 22:42:00 +0100

    fixed class Lottery

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,9 +3,14 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    public static Ball getRandomBall() {
-        String color = String.valueOf(ColorSupplier.getRandomColor());
-        int number = new Random().nextInt(101);
+
+    public static final int MAX_NUMBER = 101;
+    public final Random random = new Random();
+    public final ColorSupplier supplier = new ColorSupplier();
+
+    public Ball getRandomBall() {
+        Colors color = supplier.getRandomColor();
+        int number = random.nextInt(MAX_NUMBER);
         return new Ball(color, number);
     }
 }
